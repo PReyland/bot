4d926a2f0659821ff8f6dccba2b8310f486e2f8d
Phillip Reyland <phillip@bytedept.com>
2017-02-10 14:17:52 -0500

    Initial
    Initial

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+	consumer_key: 			'DKOvChr8tsDDMmPflAodU9vXu',
+	consumer_secret: 		'04uU4O8u7JRFVaZhCIA8ojROx5hfqvMDgIti5nFpIvTL82PirR',
+	access_token: 			'826093410197057538-kXvp8KlRkNl9cNqZC175S53fyYg4Ziy',
+	access_token_secret: 	'wo0GO1CODX5Bq2P5bufdal3PJ4mJbxItMVQGGn4CPbnyt'
+}
